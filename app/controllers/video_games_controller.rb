258c31aa1a298da204d3146de691425a815b18af
MisterAnthropy <tjcroley@outlook.com>
2020-06-07 19:06:42 -0700

    added post for new game entries

--- app/controllers/video_games_controller.rb
+++ app/controllers/video_games_controller.rb
@@ -4,6 +4,20 @@ class VideoGamesController < ApplicationController
         erb :'/games/new'
     end
 
+    post '/games' do 
+        if !logged_in?
+            redirect '/'
+        end
+        if params[:title] != ""
+            @game = VideoGames.create(title: params[:title], user_id: current_user.id)
+            redirect "/games/#{@game.id}"
+        else
+            redirect '/games/new'
+        end
+
+
+    end
+
 
 
 
